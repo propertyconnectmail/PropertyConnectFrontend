f83a796b024bfea3f51e4649ea96e180cd8f69f3
propertyconnectmail <propertyconnectmail@gmail.com>
2025-05-14 04:12:28 +0530

    Updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api'
+    apiUrl: 'https://propertconnectbackend.onrender.com/api'
+    // apiUrl: 'http://localhost:5000/api'
 };
   
